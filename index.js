d953a741147f63fe2d3cc66dfc72458798328859
CandyTale55 <candytale55@gmail.com>
2026-06-13 12:32:47 +0200

    Add .env with DB conection environment variable (not tracked) + .env.example file and update index.js to use JSON response for greeting

--- index.js
+++ index.js
@@ -4,9 +4,15 @@ const app = express();
 // Middleware to parse JSON requests
 app.use(express.json());
 
+
+// Routes
 app.use('/saludar', (req, res) => {
-    res.send('¡Hola! Bienvenido a nuestro servidor Express.');
+    //res.send('¡Hola! Bienvenido a nuestro servidor Express.');
+    return res.status(200).json({ message: '¡Hola! Bienvenido a nuestro servidor Express.' });
 });
+
+
+
 
 app.listen(3000, () => {
     console.log('Servidor levantado en http://localhost:3000');
