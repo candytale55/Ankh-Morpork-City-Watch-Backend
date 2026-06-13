cd8eefb9925d637cd02081f57c6c4b8f84c20701
CandyTale55 <candytale55@gmail.com>
2026-06-13 11:59:55 +0200

    Add express as a dependency and update package.json. Add initial code to index.js (app.listen)

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("Hello, World!");
+const express = require('express');
+const app = express();
+
+// Middleware to parse JSON requests
+app.use(express.json());
+
+app.listen(3000, () => {
+    console.log('http://localhost:3000');
+});
