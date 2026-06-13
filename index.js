6a701934dfb9f92ef83eb18124956183d037025b
CandyTale55 <candytale55@gmail.com>
2026-06-13 12:10:14 +0200

    Add first route: greeting endpoint (/saludar) in index.js

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const app = express();
 // Middleware to parse JSON requests
 app.use(express.json());
 
+app.use('/saludar', (req, res) => {
+    res.send('¡Hola! Bienvenido a nuestro servidor Express.');
+});
+
 app.listen(3000, () => {
-    console.log('http://localhost:3000');
+    console.log('Servidor levantado en http://localhost:3000');
 });
