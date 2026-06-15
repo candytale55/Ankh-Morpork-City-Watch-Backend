209f3f398501aa56e55315b63ba0f0bdb9e093d6
CandyTale55 <candytale55@gmail.com>
2026-06-15 14:52:32 +0200

    Update .gitignore, add characters data for the seed, and remove unused .gitkeep file

---
+++ src/api/data/characters.data.js
@@ -0,0 +1,74 @@
+const characters = [
+    {
+        "name": "Samuel Vimes",
+        "title": "Commander of the City Watch",
+        "organization": "Ankh-Morpork City Watch",
+        "gender": "male",
+        "species": "human"
+    },
+    {
+        "name": "Carrot Ironfoundersson",
+        "title": "Captain",
+        "organization": "Ankh-Morpork City Watch",
+        "gender": "male",
+        "species": "human"
+    },
+    {
+        "name": "Angua von Überwald",
+        "title": "Captain",
+        "organization": "Ankh-Morpork City Watch",
+        "gender": "female",
+        "species": "werewolf"
+    },
+    {
+        "name": "Fred Colon",
+        "title": "Sergeant",
+        "organization": "Ankh-Morpork City Watch",
+        "gender": "male",
+        "species": "human"
+    },
+    {
+        "name": "Nobby Nobbs",
+        "title": "Corporal",
+        "organization": "Ankh-Morpork City Watch",
+        "gender": "male",
+        "species": "human"
+    },
+    {
+        "name": "Detritus",
+        "title": "Sergeant",
+        "organization": "Ankh-Morpork City Watch",
+        "gender": "male",
+        "species": "troll"
+    },
+    {
+        "name": "Cheery Littlebottom",
+        "title": "Forensic Alchemist",
+        "organization": "Ankh-Morpork City Watch",
+        "gender": "female",
+        "species": "dwarf"
+    },
+    {
+        "name": "Reg Shoe",
+        "title": "Constable",
+        "organization": "Ankh-Morpork City Watch",
+        "gender": "male",
+        "species": "zombie"
+    },
+    {
+        "name": "Dorfl",
+        "title": "Constable",
+        "organization": "Ankh-Morpork City Watch",
+        "gender": "non-binary",
+        "species": "golem"
+    },
+    {
+        "name": "Sally von Humperdinck",
+        "title": "Constable",
+        "organization": "Ankh-Morpork City Watch",
+        "gender": "female",
+        "species": "vampire"
+    }
+]
+
+module.exports = characters;
