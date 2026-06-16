baaf6b2063d9892e03c16c4beedf821403c1ef91
CandyTale55 <candytale55@gmail.com>
2026-06-16 09:07:07 +0200

    Implement CRUD operations for Book model in book.controller.js

---
+++ src/api/controllers/book.controller.js
@@ -0,0 +1,52 @@
+// CRUD - Create, Read, Update, Delete
+const express = require('express');
+const Book = require('../models/Book');
+
+const getBooks = async (req, res) => {
+    try {
+        const books = await Book.find();
+        return res.status(200).json(books);
+    } catch (error) {
+
+        return res.status(400).json("Error in getting Books")
+    }
+};
+
+const postBook = async (req, res) => {
+    try {
+        const newBook = new Book(req.body);
+        const savedBook = await newBook.save();
+        return res.status(201).json(savedBook);
+    } catch (error) {
+        return res.status(400).json("Error in creating Book");
+    }
+};
+
+const updateBook = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const newBook = new Book(req.body);
+        newBook._id = id;
+        const updatedBook = await Book.findByIdAndUpdate(id, newBook, { new: true });
+        return res.status(200).json(updatedBook);
+    } catch (error) {
+        return res.status(400).json("Error in updating Book");
+    }
+};
+
+const deleteBook = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const deletedBook = await Book.findByIdAndDelete(id);
+        return res.status(200).json({ message: "Book deleted successfully", element: deletedBook });    
+    } catch (error) {
+        return res.status(400).json("Error in deleting Book");
+    }
+ };
+
+module.exports = {
+    getBooks,
+    postBook,
+    updateBook,
+    deleteBook
+};
