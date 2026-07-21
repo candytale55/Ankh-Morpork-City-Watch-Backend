d8224c3dc88cab58ea9a4a3e6b17e5b1150e814f
CandyTale55 <candytale55@gmail.com>
2026-07-21 15:20:40 +0200

    Remove agents from static seed data to create them in insomnia

--- src/api/data/agents.data.js
+++ src/api/data/agents.data.js
@@ -26,22 +26,6 @@ const agents = [
         "image": "https://res.cloudinary.com/zaeweyxf/image/upload/v1783317493/Angua_von_Uberwald_hsqnbj.jpg"
     },
     {
-        "name": "Fred Colon",
-        "title": "Sergeant",
-        "organization": "Ankh-Morpork City Watch",
-        "gender": "male",
-        "species": "human",
-        "image": "https://res.cloudinary.com/zaeweyxf/image/upload/v1783317494/Fred_Colon_r1tk6p.jpg"
-    },
-    {
-        "name": "Nobby Nobbs",
-        "title": "Corporal",
-        "organization": "Ankh-Morpork City Watch",
-        "gender": "male",
-        "species": "human",
-        "image": "https://res.cloudinary.com/zaeweyxf/image/upload/v1783317495/Nobby_Nobbs_s8tsbz.jpg"
-    },
-    {
         "name": "Detritus",
         "title": "Sergeant",
         "organization": "Ankh-Morpork City Watch",
@@ -56,30 +40,6 @@ const agents = [
         "gender": "female",
         "species": "dwarf",
         "image": "https://res.cloudinary.com/zaeweyxf/image/upload/v1783317494/Cheery_Littlebottom_pdybzr.jpg"
-    },
-    {
-        "name": "Reg Shoe",
-        "title": "Constable",
-        "organization": "Ankh-Morpork City Watch",
-        "gender": "male",
-        "species": "zombie",
-        "image": "https://res.cloudinary.com/zaeweyxf/image/upload/v1783317493/Reg_Shoe_h7gkvx.jpg"
-    },
-    {
-        "name": "Dorfl",
-        "title": "Constable",
-        "organization": "Ankh-Morpork City Watch",
-        "gender": "non-binary",
-        "species": "golem",
-        "image": "https://res.cloudinary.com/zaeweyxf/image/upload/v1783317494/Dorfl_fneebn.jpg"
-    },
-    {
-        "name": "Sally von Humperdinck",
-        "title": "Constable",
-        "organization": "Ankh-Morpork City Watch",
-        "gender": "female",
-        "species": "vampire",
-        "image": "https://res.cloudinary.com/zaeweyxf/image/upload/v1783317493/Sally_von_Humperdinck_visible_j0fe2a.jpg"
     }
 ]
 
